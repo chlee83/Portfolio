293cdb54942a79e02b55b40f7c8f359c8a3382c7
Hobbes <chilee83@gmail.com>
2019-03-25 11:19:11 -0400

    update portfolio

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
 
+    $(".headerButton").hover(function() {
+        $('.name').animate({width: '100px'}, 500)
+    }, function(){
+        $('.name').animate({width: '0'}, 1000)
+    });
 
 
 
