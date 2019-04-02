c3f5fb92a1ee5d5a7de89017ea73edfdc6f9e737
Hobbes <chilee83@gmail.com>
2019-04-02 09:07:59 -0400

    added liri

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,22 +1,12 @@
 $(document).ready(function() {
 
 
+    //mouse hover over main header reveals full name
     $(".headerButton").hover(function() {
         $('.name').animate({width: '100px'}, 500)
     }, function(){
         $('.name').animate({width: '0'}, 1000)
     });
-
-
-
-
-
-
-
-
-
-
-
 
 
 
